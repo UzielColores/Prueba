d7bf94581d873ae394186d6f1aa791f5e2447a7e
Bruno Jardon <bjardon97@gmail.com>
2019-02-12 10:49:32 -0600

    modifiqué el archivo desde otra pc ja ja

--- hola.cpp
+++ hola.cpp
@@ -5,5 +5,7 @@ using namespace std;
 int main (){
     cout<<"El que quieras"<<endl;
     cout<<"Estoy jugando con git"<<endl;
+    cout<<"ja ja  ahora lo moidifco desde la lap de prry"<<endl;
+    
     return 0;
 }
